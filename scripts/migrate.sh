8184d69766c51eb7bc846c8c436af2571020499d
Balaramakrishna Alti <48998457+balaramaa@users.noreply.github.com>
2026-03-18 10:28:17 -0400

    Update migrate.sh

--- scripts/migrate.sh
+++ scripts/migrate.sh
@@ -1,1 +1,18 @@
+#!/bin/bash
 
+echo "Stopping VASD..."
+/opt/quest/bin/vastool stop
+
+echo "Uninstalling VASD..."
+rpm -e vasd
+
+echo "Installing SSSD..."
+yum install -y sssd
+
+echo "Configuring SSSD..."
+cp sssd.conf /etc/sssd/sssd.conf
+chmod 600 /etc/sssd/sssd.conf
+
+echo "Starting SSSD..."
+systemctl enable sssd
+systemctl start sssd
